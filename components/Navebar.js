95144ca41917d4e98193c1aa60cd2b6e2c996d6e
Mahbub Hasan <38033407+xmahbub@users.noreply.github.com>
2022-10-06 22:06:11 +0600

    .
    .

--- components/Navebar.js
+++ components/Navebar.js
@@ -6,7 +6,7 @@ import { motion } from "framer-motion"
 
 function Navebar() {
   return (
-    <div className="bg-body-texture background-size:cover bg-contain flex navbar justify-between bg-base-100 pl-4 shadow-xl">
+    <div className="bg-body-texture background-size:cover bg-contain flex navbar justify-between bg-base-100 shadow-xl">
        
         <button className="btn btn-square btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current text-orange-600"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
